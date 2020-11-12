5c9fe1bb0f398ff0a4871b393aa711ca08be9417
Alberto <thebertsinfo@gmail.com>
2020-11-12 15:18:37 +0100

    Aggiunti elementi con Vue da array su sezione user-contacts

--- main.js
+++ main.js
@@ -1,9 +1,164 @@
 var app = new Vue({
     el: '#root',
     data: {
-
+        contact: [
+            {
+                name: 'Michele',
+                avatar: 'img/avatar_1.png',
+                stato: 'online',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Sara',
+                avatar: 'img/avatar_8.png',
+                stato: 'online',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Darth Maul',
+                avatar: 'img/avatar_5.png',
+                stato: 'online',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Luke',
+                avatar: 'img/avatar_6.png',
+                stato: 'online',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Frederick',
+                avatar: 'img/avatar_3.png',
+                stato: 'online',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Sandra',
+                avatar: 'img/avatar_7.png',
+                stato: 'online',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+            {
+                name: 'Erick',
+                avatar: 'img/avatar_2.png',
+                stato: 'online',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+        ],
     },
     methods: {
-        
+
     }
 });
